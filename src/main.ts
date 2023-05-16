98646347f9a431983a2b250ff1cb0dcb53ccd4df
Stanislav <stanislaworonin@yandex.ru>
2023-05-16 09:09:12 +0300

    UPD: configurated main fail | add swagger and logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Posts')
+    .setDescription('CRUD API')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, () => {
+    Logger.log(`Application started on http://localhost:${port}.`, 'Main');
+    Logger.log(
+      `Swagger documentation on http://localhost:${port}/api-doc.`,
+      'Main',
+    );
+  });
 }
 bootstrap();
